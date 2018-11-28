74ae54727c8623d1819c61da6cb005e472205a47
Musa <musa@iMac10.local>
2018-11-28 08:54:16 -0500

    did not start

--- card.py
+++ card.py
@@ -1,8 +1,24 @@
-class card:
-    def __init__(self, rank, suit):
+import card
+import random
 
+class Card:
+    def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+        self.ranks = ["Ace", "two", "Three" "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+        self.suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 
+    def compared_to(self, other_card):
+        if self.rank > other_card.rank:
+            return other_card
+        elif other_card.rank > self.rank:
+            return other_card
+        else:
+            if self.suit > other_card.suit:
+                return self
+            else:
+                return card
 
-    
+    def __str__(self):
+        new_card = ranks[self.rank] + "of" + suits[self.suit]
+        return new_card
